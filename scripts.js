406966426b631562ad0832f59a667ba4dd215c7e
Jasio <mcyanek@gmail.com>
2020-07-05 22:34:09 +0200

    implements interactive elements

---
+++ scripts.js
@@ -0,0 +1,24 @@
+document.getElementById('CBR').onmouseover = function () {
+    document.getElementById('CBR').className = "mouseover";
+}
+
+document.getElementById('CBR').onmouseleave = function () {
+    document.getElementById('CBR').className = "mousedown";
+}
+
+document.getElementById('SPRT').onmouseover = function () {
+    document.getElementById('SPRT').className = "mouseover";
+}
+
+document.getElementById('SPRT').onmouseleave = function () {
+    document.getElementById('SPRT').className = "mousedown";
+}
+
+document.getElementById('IRL').onmouseover = function () {
+    document.getElementById('IRL').className = "mouseover";
+}
+
+document.getElementById('IRL').onmouseleave = function () {
+    document.getElementById('IRL').className = "mousedown";
+}
+
